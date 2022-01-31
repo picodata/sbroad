4f13ef13c31ab3a7f0dacba930d6c6e473683387
Denis Smirnov <sd@picodata.io>
2022-01-31 11:49:15 +0700

    test: no motions for the local sub-query

--- src/ir/transformation/redistribution/tests.rs
+++ src/ir/transformation/redistribution/tests.rs
@@ -185,3 +185,49 @@ fn full_motion_non_segment_outer_for_sub_query() {
     let expected_plan = Plan::from_yaml(&s).unwrap();
     assert_eq!(plan, expected_plan);
 }
+
+#[test]
+fn local_sub_query() {
+    // t1(a int) key [a]
+    // t2(a int, b int) key [a]
+    // select * from t1 where a = (select a from t2)
+    let mut plan = Plan::new();
+    let mut children: Vec<usize> = Vec::new();
+
+    let t1 = Table::new_seg("t1", vec![Column::new("a", Type::Integer)], &["a"]).unwrap();
+    plan.add_rel(t1);
+    let scan_t1_id = plan.add_scan("t1").unwrap();
+    children.push(scan_t1_id);
+
+    let t2 = Table::new_seg("t2", vec![Column::new("a", Type::Integer)], &["a"]).unwrap();
+    plan.add_rel(t2);
+    let scan_t2_id = plan.add_scan("t2").unwrap();
+    let proj_id = plan.add_proj(scan_t2_id, &["a"]).unwrap();
+    let sq_id = plan.add_sub_query(proj_id, None).unwrap();
+    children.push(sq_id);
+
+    let id = plan.nodes.next_id();
+    let inner_a_id = plan
+        .add_row_from_sub_query(id, &children[..], children.len() - 1, &["a"])
+        .unwrap();
+    let outer_a_id = plan.add_row_from_child(id, scan_t1_id, &["a"]).unwrap();
+    let eq_id = plan.add_bool(outer_a_id, Bool::Eq, inner_a_id).unwrap();
+
+    let select_id = plan.add_select(&children[..], eq_id, id).unwrap();
+    plan.set_top(select_id).unwrap();
+
+    plan.add_motions().unwrap();
+
+    // Check the modified plan
+    plan.nodes.add_new_equalities().unwrap();
+    let path = Path::new("")
+        .join("tests")
+        .join("artifactory")
+        .join("ir")
+        .join("transformation")
+        .join("redistribution")
+        .join("local_sub_query.yaml");
+    let s = fs::read_to_string(path).unwrap();
+    let expected_plan = Plan::from_yaml(&s).unwrap();
+    assert_eq!(plan, expected_plan);
+}
