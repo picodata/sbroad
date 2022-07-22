f30c62e33f3f8c2791875485324d0e4c202d9ee6
Denis Smirnov <sd@picodata.io>
2022-07-22 17:19:05 +0700

    feat(stress test): improve the amount of routers

--- stress-test/target_data/scenario.js
+++ stress-test/target_data/scenario.js
@@ -1,7 +1,23 @@
 import tarantool from "k6/x/tarantool";
 import {randomItem} from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
 
-const client = tarantool.connect("localhost:3301", {"user": "admin", pass: "app-cluster-cookie"})
+const clients = [
+    tarantool.connect("localhost:3301", {"user": "admin", pass: "app-cluster-cookie"}),
+    tarantool.connect("localhost:3306", {"user": "admin", pass: "app-cluster-cookie"}),
+    tarantool.connect("localhost:3307", {"user": "admin", pass: "app-cluster-cookie"}),
+    tarantool.connect("localhost:3308", {"user": "admin", pass: "app-cluster-cookie"})
+]
+
+export let current_server = 0
+
+function get_client() {
+    let c = clients[current_server]
+    current_server += 1
+    if (current_server >= clients.length) {
+        current_server = 0
+    }
+    return c
+}
 
 let ids = Array.from(
     {
@@ -21,5 +37,5 @@ const pattern = `SELECT *
     WHERE "reestrid" = ?`
 
 export default () => {
-    tarantool.call(client, "query", [pattern, [randomItem(ids)]]);
+    tarantool.call(get_client(), "query", [pattern, [randomItem(ids)]]);
 }
