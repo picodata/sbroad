04f8ee503d1143d6b153f8fec38c0ad5267f21ef
Denis Smirnov <sd@picodata.io>
2023-07-21 13:03:05 +0700

    feat: extend DDL node API with 'take_ddl_node()' call

--- sbroad-core/src/ir/ddl.rs
+++ sbroad-core/src/ir/ddl.rs
@@ -81,4 +81,22 @@ impl Plan {
             )),
         }
     }
+
+    /// Take DDL node from the plan arena and replace it with parameter node.
+    ///
+    /// # Errors
+    /// - current node is not of DDL type
+    pub fn take_ddl_node(&mut self, node_id: usize) -> Result<Ddl, SbroadError> {
+        // Check that node is DDL type (before making any distructive operations).
+        let _ = self.get_ddl_node(node_id)?;
+        // Replace DDL with parameter node.
+        let node = std::mem::replace(self.get_mut_node(node_id)?, Node::Parameter);
+        match node {
+            Node::Ddl(ddl) => Ok(ddl),
+            _ => Err(SbroadError::Invalid(
+                Entity::Node,
+                Some(format!("node is not DDL type: {node:?}")),
+            )),
+        }
+    }
 }
